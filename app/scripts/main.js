2f50a143542451bb1c717b99207ae556dc8666e8
tyhus14 <huston.tyler3@gmail.com>
2014-05-11 16:34:11 -0400

    added about and featured sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(".nav-items").bind("click", function(e) {
+
+    e.preventDefault();
+    var location = $(this).attr("href")
+
+    $('html, body').animate({
+        scrollTop: $(location).offset().top
+    }, 1500);
+
+});
